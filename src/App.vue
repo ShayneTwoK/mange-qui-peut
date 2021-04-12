46709ac7299caa61b79ba235d0560364cf08c0a8
Shaïnee <shaynetwok@gmail.com>
2021-04-13 01:26:59 +0200

    bug de derniere minute css vuetify manquant

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <v-app-bar
       app
       color="primary"
-      dark>
-      
+      dark
+    >
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -15,21 +15,44 @@
           width="40"
         />
 
-        <h1><router-link style="color:white; text-decoration:none" to="/">Mange qui peut</router-link></h1>
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
       </div>
 
       <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld';
+
 export default {
   name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
   data: () => ({
     //
   }),
